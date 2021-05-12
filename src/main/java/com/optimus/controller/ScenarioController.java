b18a7029d626e381c0f5d8238c71970d94004e89
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2021-05-12 21:14:30 +0300

    Get scenario by id

--- src/main/java/com/optimus/controller/ScenarioController.java
+++ src/main/java/com/optimus/controller/ScenarioController.java
@@ -2,6 +2,7 @@ package com.optimus.controller;
 
 import com.optimus.dto.PageDto;
 import com.optimus.dto.ScenarioDto;
+import com.optimus.entity.Scenario;
 import com.optimus.service.ScenarioService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -27,5 +29,12 @@ public class ScenarioController {
         if (page.getNumber() + 1 > page.getTotalPages() && page.getTotalPages() > 0)
             throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
         return ResponseEntity.ok(new PageDto<ScenarioDto>(page));
+    }
+
+    @GetMapping("/api/scenarios/{id}")
+    @ApiOperation("Get scenario by id")
+    public ResponseEntity<ScenarioDto> getScenarioById(@PathVariable Long id) {
+        Scenario scenario = scenarioService.findById(id).orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(new ScenarioDto(scenario));
     }
 }
